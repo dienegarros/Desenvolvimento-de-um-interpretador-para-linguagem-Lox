ad228608da2272a935245190f7cca9281a88041c
dienegarros <diene.ivana@discente.ufma.br>
2025-06-07 18:08:40 -0300

    ajuste frase analisada main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from lox.scanner import Scanner
 
 if __name__ == "__main__":
-    fonte = 'var nome = "Diene"; print(nome); // comentário'
+    fonte = 'var nome = "Diene e Diele vao passar em compiladores";'
     scanner = Scanner(fonte)
     tokens = scanner.scan_tokens()
     for token in tokens:
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 ## Integrante
 - Diene Ivana Coelho Cantanhede - [@dienegarros](https://github.com/dienegarros)
-- Diele Ilana Coelho Cantanhede - 
+- Diele Ilana Coelho Cantanhede - [@dielecoelho](https://github.com/DieleCoelho)
